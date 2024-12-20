eec41ab3766891579f980b797feabcfc2281b4cf
jyb-maker <bjy7328@naver.com>
2024-12-20 16:05:20 +0900

    Autowired 기능 추가 / writeok 메소드 추가

---
+++ src/main/java/com/jbedu/board/controller/BoardController.java
@@ -0,0 +1,42 @@
+package com.jbedu.board.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jbedu.board.dao.BoardDao;
+import com.jbedu.board.util.Constant;
+
+@Controller
+public class BoardController {
+	
+	private JdbcTemplate template;
+	
+	@Autowired
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+		Constant.template = this.template;
+	}
+
+	@RequestMapping(value = "/write_form")
+	public String write_form() {
+		return "write_form";
+	}
+	
+	@RequestMapping(value = "/writeOk")
+	public String writeOk(HttpServletRequest request, Model model) {
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");
+		
+		BoardDao boardDao = new BoardDao();
+		boardDao.boardWrite(bname, btitle, bcontent);
+		
+		return "redirect:boardList";
+	}
+	
+}
---
+++ src/main/java/com/jbedu/board/util/Constant.java
@@ -0,0 +1,9 @@
+package com.jbedu.board.util;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Constant {
+
+	public static JdbcTemplate template; //전역변수
+	
+}
